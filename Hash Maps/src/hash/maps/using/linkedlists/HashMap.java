40e94c545ff1d7ff8b71c867caf731bc60adc0cc
s8189882 <s8189882@gmail.com>
2022-09-10 15:13:19 +0530

    create master branch

---
+++ Hash Maps/src/hash/maps/using/linkedlists/HashMap.java
@@ -0,0 +1,9 @@
+package hash.maps.using.linkedlists;
+
+public class HashMap{
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Hash Table data structure!");
+	}
+}
